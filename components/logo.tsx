9edbbe2deaa6646f7b93b0327f02ec3ecd0b11f3
Zwyx <29386932+Zwyx@users.noreply.github.com>
2025-09-28 12:31:25 +0800

    Fix title alignment

--- components/logo.tsx
+++ components/logo.tsx
@@ -6,8 +6,8 @@ export function Logo() {
 			href="/"
 			className="flex items-center space-x-2 transition-opacity hover:opacity-80"
 		>
-			<div className="flex flex-col">
-				<span className="font-playfair text-foreground text-xl font-semibold text-[#6614b8]/60">
+			<div className="flex flex-col items-center">
+				<span className="font-playfair text-foreground text-xl font-semibold">
 					Florence Viprey
 				</span>
 				<span className="font-playfair text-sm font-medium text-[#6614b8]/40">
