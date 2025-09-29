71528a3cac2ab13214073e58a0f68c6fb6ec3097
Zwyx <29386932+Zwyx@users.noreply.github.com>
2025-09-29 17:02:36 +0800

    Add Plausible script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import type React from "react";
+import { LanguageProvider } from "@/hooks/use-language";
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import Script from "next/script";
+import type React from "react";
 import { Suspense } from "react";
 import "./globals.css";
-import { LanguageProvider } from "@/hooks/use-language";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -18,10 +19,9 @@ const playfair = Playfair_Display({
 });
 
 export const metadata: Metadata = {
-	title: "Florence Viprey - Portfolio",
+	title: "Florence Viprey",
 	description:
 		"Chef de projet en Recherche et Développement en Agro-alimentaire",
-	generator: "v0.app",
 };
 
 export default function RootLayout({
@@ -30,12 +30,18 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
+		// TODO: ensure `lang` is updated when changing language
 		<html lang="fr" className={`${inter.variable} ${playfair.variable}`}>
 			<body className="font-sans antialiased">
 				<LanguageProvider>
 					<Suspense fallback={null}>{children}</Suspense>
 				</LanguageProvider>
 			</body>
+			<Script
+				src="https://plsbl.zwyx.dev/js/script.js"
+				data-domain="florenceviprey.com"
+				defer
+			/>
 		</html>
 	);
 }
